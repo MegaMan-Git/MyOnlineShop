a0589eca105c6b2af6ec48e468d6ce68eef5f4e9
amirmohammad <rafieyg2708@gmail.com>
2026-08-28 10:15:42 +0330

    Add_AuthRepo

---
+++ OnlineShop/Application/Interfaces/Repositories/IAuthRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IAuthRepository
+    {
+        public Task<bool> FindUser();
+
+        public Task<bool> SignIn();
+
+        public Task<string> RestPassword_CreateToken();
+
+        public Task<string> RestPassword();
+
+    }
+}
---
+++ OnlineShop/Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces.Repositories;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public AuthRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<bool> FindUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RestPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RestPassword_CreateToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignIn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
