4c706e0e1154b3e09eaaade360e40422e06c9d86
amirmohammad <rafieyg2708@gmail.com>
2026-09-20 15:48:09 +0330

    Fix TokenEncoder

--- OnlineShop/Infrastructure/Services/TokenEncoder.cs
+++ OnlineShop/Infrastructure/Services/TokenEncoder.cs
@@ -9,12 +9,12 @@ namespace Infrastructure.Services
 {
     public class TokenEncoder : ITokenEncoder
     {
-        public string Decode(string token)
+        public string Encode(string token)
         {
             return WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
         }
 
-        public string Encode(string token)
+        public string Decode(string token)
         {
             return Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(token));
         }
